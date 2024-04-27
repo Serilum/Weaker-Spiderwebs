165901d98524e23c732a48943518c210c4cfe1d6
Rick South <rick@natamus.com>
2024-04-28 01:20:04 +0200

    Weaker Spiderwebs source code for 1.20.5.

---
+++ NeoForge/src/main/java/com/natamus/weakerspiderwebs/neoforge/events/NeoForgeWebEvent.java
@@ -0,0 +1,25 @@
+package com.natamus.weakerspiderwebs.neoforge.events;
+
+import com.natamus.weakerspiderwebs.events.WebEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeWebEvent {
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide || !e.phase.equals(Phase.END)) {
+			return;
+		}
+
+		WebEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+}
