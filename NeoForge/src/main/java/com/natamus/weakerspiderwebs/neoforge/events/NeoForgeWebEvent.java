6c7fe42828878a7489b28f3b99ac20d8188c268c
Rick South <rick@natamus.com>
2024-04-28 18:55:54 +0200

    (v3.6) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/weakerspiderwebs/neoforge/events/NeoForgeWebEvent.java
+++ NeoForge/src/main/java/com/natamus/weakerspiderwebs/neoforge/events/NeoForgeWebEvent.java
@@ -5,18 +5,17 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent.Phase;
-import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 
 @EventBusSubscriber
 public class NeoForgeWebEvent {
 	@SubscribeEvent
-	public static void onPlayerTick(PlayerTickEvent e) {
-		Player player = e.player;
+	public static void onPlayerTick(PlayerTickEvent.Post e) {
+		Player player = e.getEntity();
 		Level level = player.level();
-		if (level.isClientSide || !e.phase.equals(Phase.END)) {
+		if (level.isClientSide) {
 			return;
 		}
 
