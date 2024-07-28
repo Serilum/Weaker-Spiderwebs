5b47bd313a6233d0068601bc5178505193d45fc0
Rick South <rick@natamus.com>
2024-07-28 07:09:38 +0200

    (v3.7) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/weakerspiderwebs/util/Reference.java
+++ Common/src/main/java/com/natamus/weakerspiderwebs/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.weakerspiderwebs.util;
 public class Reference {
 	public static final String MOD_ID = "weakerspiderwebs";
 	public static final String NAME = "Weaker Spiderwebs";
-	public static final String VERSION = "3.4";
+	public static final String VERSION = "3.7";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
