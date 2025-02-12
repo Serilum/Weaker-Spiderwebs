855fa665e8a1351335670be765e1a6c3797f59b2
Rick South <rick@natamus.com>
2025-02-12 23:07:48 +0100

    (v3.8) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/weakerspiderwebs/ModForge.java
+++ Forge/src/main/java/com/natamus/weakerspiderwebs/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeWebEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeWebEvent.class);
 	}
 
 	private static void setGlobalConstants() {
